df70a9cb796e64cfd8e0c37fd4ad7897aa22cd15
liubaterehova <liubov.terehova@gmail.com>
2019-11-26 15:26:30 +0300

    made form

--- src/components/pages/InputPage/InputPage.jsx
+++ src/components/pages/InputPage/InputPage.jsx
@@ -1,27 +1,89 @@
 import React, { Component } from "react";
-import { AutoComplete } from "antd";
+import { AutoComplete, Descriptions } from "antd";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
 export default class InputPage extends Component {
   state = {
-    countryName: []
+    countryName: "",
+    capital: "",
+    weatherFor5Days: null,
+    population: null,
+    code: null,
+    translations: [],
+    coords: "",
+    holidays: ""
   };
+
   getCountriesName = countries => {
     return countries.map(country => country.name);
   };
+
   componentDidMount() {
     this.props.getCountries();
   }
+
   onSearch = (value, nameOfCountries) => {
     if (!nameOfCountries.includes(value)) return;
+    const searchObjIndex = nameOfCountries.indexOf(value);
+    console.log("index", searchObjIndex);
+    const country = this.props.countries[searchObjIndex];
+    console.log(country);
+
     return this.setState({
-      countryName: value
+      countryName: value,
+      capital: country.capital,
+      population: country.population,
+      code: country.numericCode,
+      translations: country.translations,
+      coords: country.latlng
     });
   };
+
+  makeBeautifulTranslate(arr) {
+    let keys = Object.keys(arr);
+    console.log("keys", keys);
+    let newarr = keys.map(key => `${key}: ${arr[key]}`);
+    console.log(newarr);
+    return this.makeArr(newarr);
+  }
+  makeArr(arr) {
+    return arr.map(item => <li>{item}</li>);
+  }
+
   render() {
     const { countries, ...otherProps } = this.props;
     console.log("this.state", this.state);
     const nameOfCountries = this.getCountriesName(countries);
+    if (this.state.countryName) {
+      const {
+        countryName,
+        capital,
+        weatherFor5Days,
+        population,
+        code,
+        translations,
+        coords,
+        holidays
+      } = this.state;
+
+      return (
+        <Descriptions title="Country Info">
+          <Descriptions.Item label="Country">{countryName}</Descriptions.Item>
+          <Descriptions.Item label="Capital">{capital}</Descriptions.Item>
+          <Descriptions.Item label="Weather">
+            {weatherFor5Days}
+          </Descriptions.Item>
+          <Descriptions.Item label="Translations">
+            {this.makeBeautifulTranslate(translations)}
+          </Descriptions.Item>
+          <Descriptions.Item label="Code">{code}</Descriptions.Item>
+          <Descriptions.Item label="Population">{population}</Descriptions.Item>
+          <Descriptions.Item label="coords">
+            {this.makeArr(coords)}
+          </Descriptions.Item>
+        </Descriptions>
+      );
+    }
     return (
       <AutoComplete
         style={{ width: 200 }}
