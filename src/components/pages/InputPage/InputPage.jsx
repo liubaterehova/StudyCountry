a2e0cde1b27ae2139fc17514cf90505234381008
liubaterehova <liubov.terehova@gmail.com>
2019-11-26 13:19:19 +0300

    added in state nameOfCountry

--- src/components/pages/InputPage/InputPage.jsx
+++ src/components/pages/InputPage/InputPage.jsx
@@ -3,24 +3,31 @@ import { AutoComplete } from "antd";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
 export default class InputPage extends Component {
+  state = {
+    countryName: []
+  };
   getCountriesName = countries => {
     return countries.map(country => country.name);
   };
   componentDidMount() {
     this.props.getCountries();
-    console.log("arrayOfCountries", this.props.countries);
   }
-
+  onSearch = (value, nameOfCountries) => {
+    if (!nameOfCountries.includes(value)) return;
+    return this.setState({
+      countryName: value
+    });
+  };
   render() {
     const { countries, ...otherProps } = this.props;
-    console.log("countriesInputPage", countries);
-    console.log("this.props in InputPage", this.props);
+    console.log("this.state", this.state);
     const nameOfCountries = this.getCountriesName(countries);
     return (
       <AutoComplete
         style={{ width: 200 }}
         dataSource={nameOfCountries}
         placeholder="write country"
+        onSearch={value => this.onSearch(value, nameOfCountries)}
         filterOption={(inputValue, option) =>
           option.props.children
             .toUpperCase()
