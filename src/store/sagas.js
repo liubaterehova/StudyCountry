8049ad2954844c690d74154ce9ead459e9f49e5b
liubaterehova <liubov.terehova@gmail.com>
2019-12-02 19:29:46 +0300

    d

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -2,6 +2,5 @@ import { all } from "redux-saga/effects";
 import customSaga from "./custom/sagas";
 
 export default function* rootSaga() {
-    console.log("customSaga 2", customSaga);
     yield all([...customSaga]);
 }
